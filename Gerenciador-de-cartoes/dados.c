d33bffc4e2a44ddb931bb13a9e7cbbf0f8d0f694
Daniel Gama <danielgmbr30@gmail.com>
2026-01-17 07:31:08 -0300

    feat: criando void adicionar cliente

--- Gerenciador-de-cartoes/dados.c
+++ Gerenciador-de-cartoes/dados.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "dados.h"
 
 BancoDados* inicializar_banco() {
@@ -17,4 +19,16 @@ BancoDados* inicializar_banco() {
     return b;
 }
 
-void adicionar_cliente(BancoDados *b, char *nome);
+void adicionar_cliente(BancoDados *b, char *nome) {
+    if (b->capacidade_clientes == 0) {
+        b = (BancoDados*) realloc(b, sizeof(BancoDados) * (b->capacidade_clientes += 10));
+    }
+    if (b->quant_clientes == b->capacidade_clientes) {
+        b = (BancoDados*) realloc(b, sizeof(BancoDados) * (b->capacidade_clientes *= 2));
+    }
+
+    //copiar b->clientes[b->quant_clientes].nome para onde??
+    b->clientes->id = b->quant_clientes + 1; //provavelmente algo assim?
+    b->clientes->cartoes = NULL; //assim talvez? deve ta faltando falar qual o indice do cliente
+    b->quant_clientes++;
+}
